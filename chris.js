a6cb8a27bee25b39d75403bbac82e1e22db93eff
chimbiko-xd <chimbikouzukwu202@gmail.com>
2025-05-05 16:57:50 +0100

    Create chris.js

---
+++ chris.js
@@ -0,0 +1,20 @@
+var commands = [];
+
+function chris(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    chris,
+    AddCommand:chris,
+    Function:chris,
+    Module:chris,
+    commands,
+};
