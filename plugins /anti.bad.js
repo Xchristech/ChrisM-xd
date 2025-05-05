01f794f210bcaa5eeb68de9ccd2741aa6382dbad
RealChrisX <chimbikouzukwu202@gmail.com>
2025-05-06 00:35:21 +0100

    Create anti.bad.js

---
+++ plugins /anti.bad.js
@@ -0,0 +1,34 @@
+const { chris } = require('../chris');
+const config = require("../settings");
+
+// Anti-Bad Words System
+chris({
+  'on': "body"
+}, async (conn, m, store, {
+  from,
+  body,
+  isGroup,
+  isAdmins,
+  isBotAdmins,
+  reply,
+  sender
+}) => {
+  try {
+    const badWords = ["wtf", "mia", "xxx", "fuck", 'sex', "huththa", "pakaya", 'ponnaya', "hutto"];
+
+    if (!isGroup || isAdmins || !isBotAdmins) {
+      return;
+    }
+
+    const messageText = body.toLowerCase();
+    const containsBadWord = badWords.some(word => messageText.includes(word));
+
+    if (containsBadWord && config.ANTI_BAD_WORD === "true") {
+      await conn.sendMessage(from, { 'delete': m.key }, { 'quoted': m });
+      await conn.sendMessage(from, { 'text': "😤 ⚠️ BAD WORDS NOT ALLOWED ⚠️ 😤" }, { 'quoted': m });
+    }
+  } catch (error) {
+    console.error(error);
+    reply("An error occurred while processing the message.");
+  }
+});
