17ff1dbeafbaab7cb4bef4829b4e9e4156b90bec
ChrisGaaju <chimbikouzukwu202@gmail.com>
2025-05-24 09:06:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,33 +1,138 @@
-
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-  return text === fault ? true : false;
+    return text === fault ? true : false;
 }
-
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "CHRISM_XD_SESSION",
-  // Add your Session ID; make sure it starts with "CHRISM_XD_SESSION"
-
-  PREFIX: process.env.PREFIX || ".",
-  // Add your command prefix
-
-  BOT_NAME: process.env.BOT_NAME || "CHRISM-XD",
-  // Your bot name here
-
-  MODE: process.env.MODE || "public",
-  // Options: public - private - inbox - group
-
-  OWNER_NAME: process.env.OWNER_NAME || "Chimbiko-xd",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "2348069675806",
-
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "Hello! I am CHRISM-XD WhatsApp Bot.",
-};
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id make sure it starts with chris~
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
+// add bot name here for menu
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
 
 
----
+        AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+
+        AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+
+        AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+
+        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MALVIN XD 😆*",
+// set the auto reply massage on status reply  
+
+
+       WELCOME: process.env.WELCOME || "true",
+// true if want welcome and goodbye msg in groups 
+   
+       ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+
+      ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+
+      MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+
+     MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qumhu4.jpg",
+// add custom menu and mention reply image url
+
+       ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/xshsmk",
+// add img for alive msg
+
+        LIVE_MSG: process.env.LIVE_MSG || "> ʙᴏᴛ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ\n\n\nᴋᴇᴇᴘ ᴜsɪɴɢ ✦ᴍᴀʟᴠɪɴ xᴅ✦ ғʀᴏᴍ ᴍᴀʟᴠɪɴ ᴛᴇᴄʜ ɪɴᴄ⚡\n\n\n*© ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ\n\n> ɢɪᴛʜᴜʙ :* github.com/XdKing2/MALVIN-XD",
+// add alive msg here 
+
+
+        STICKER_NAME: process.env.STICKER_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
+// type sticker pack name 
+
+        CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react  
+  
+      CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+
+          DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "26371475XXXX",
+// add your bot owner number
+
+OWNER_NAME: process.env.OWNER_NAME || "ᴍᴀʟᴠɪɴ ᴛᴇᴄʜ",
+// add bot owner name
+
+              DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ*",
+// add bot owner name    
+
+        READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+
+                 AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+                ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+
+            ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+// make anti link true,false for groups 
+
+            AUTO_VOICE: process.env.AUTO_VOICE || "false",
+// make true for send automatic voices
+
+        AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+
+              AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+
+        ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+
+         PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
+// make false if want private mod
+
+        AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing 
+  
+   READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+
+     DEV: process.env.DEV || "2348069675806",
+//replace with your whatsapp number    
     
+    ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+
+      ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+// change it to 'same' if you want to resend deleted message in same chat 
+
+      AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+// make it true for auto recoding 
+
+version: process.env.version || "1.4.2",
+
+    
+START_MSG: process.env.START_MSG || `*Hᴇʟʟᴏ ᴛʜᴇʀᴇ ᴍᴀʟᴠɪɴ xᴅ ᴄᴏɴɴᴇᴄᴛᴇᴅ! 👋🏻* 
+
+    *ᴋᴇᴇᴘ ᴏɴ ᴜsɪɴɢ ᴍᴀʟᴠɪɴ ᴍᴏᴅs🚩* 
+
+> Joɪɴ ᴡʜᴀᴛsᴀᴘᴘ ᴄʜᴀɴɴᴇʟ (ᴀ ᴍᴜsᴛ): ⤵️  
+    https://whatsapp.com/channel/0029Vb5qc6N2Jl8E3EcVBv0t
+
+> sᴜʙsᴄʀɪʙᴇ ʏᴛ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴛᴜᴛᴏʀɪᴀʟs
+    https://youtube.com/@ChrisSpark-XMD
+
+    - *ʏᴏᴜʀ ʙᴏᴛ ᴘʀᴇғɪx: ➡️[ . ]*
+> - ʏᴏᴜ ᴄᴀɴ ᴄʜᴀɴɢᴇ ᴜʀ ᴘʀᴇғɪx ᴜsɪɴɢ ᴛʜᴇ .ᴘʀᴇғɪx ᴄᴏᴍᴍᴀɴᴅ
+
+> ᴅᴏɴᴛ ғᴏʀɢᴇᴛ ᴛᴏ sʜᴀʀᴇ, sᴛᴀʀ &ғᴏʀᴋ ᴛʜᴇ ʀᴇᴘᴏ ⬇️ 
+    https://github.com/Chimbiko-xd/CHRISM-XD
+
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ 🇳🇬`
+};
